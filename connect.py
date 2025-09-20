38d1951ee9110dbe7312bcd50442b526788ba24e
Pierre Sandy <87212766+PierreSandy@users.noreply.github.com>
2025-09-21 10:14:24 +1200

    Initial commit

---
+++ connect.py
@@ -0,0 +1,7 @@
+# Database connection configuration
+dbuser = "root"
+dbpass = "your_password_here"   # <-- Enter your MySQL Server password here 
+                                # (NOT PRIVATE - don't re-use an important password you use elsewhere)
+dbhost = "127.0.0.1"
+dbport = 3306
+dbname = "svs"
