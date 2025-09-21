f2554f433c541f769507e990254e82b910076cd7
Pierre Sandy <87212766+PierreSandy@users.noreply.github.com>
2025-09-21 12:33:16 +1200

    adding db password

--- connect.py
+++ connect.py
@@ -1,6 +1,6 @@
 # Database connection configuration
 dbuser = "root"
-dbpass = "k)4fm!"   # <-- Enter your MySQL Server password here 
+dbpass = "k)4fm!oehn76"   # <-- Enter your MySQL Server password here 
                                 # (NOT PRIVATE - don't re-use an important password you use elsewhere)
 dbhost = "127.0.0.1"
 dbport = 3306
